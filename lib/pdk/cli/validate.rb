305058d89088d30d341c5c782c42466d93058fd2
Austin Blatt <austinblatt@gmail.com>
2017-07-25 11:03:14 -0700

    Negotiate locale correctly

--- lib/pdk/cli/validate.rb
+++ lib/pdk/cli/validate.rb
@@ -92,7 +92,8 @@ module PDK::CLI
         validators.each do |validator|
           Dir.chdir(PDK::Util.module_root) do
             threads << Thread.new do
-              require 'pdk/i18n'
+              GettextSetup.initialize(File.absolute_path('../../../locales', File.dirname(__FILE__)))
+              GettextSetup.negotiate_locale!(GettextSetup.candidate_locales)
               exit_codes << validator.invoke(report, options)
             end
           end
