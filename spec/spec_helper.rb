aa440f497ffa0f5ecf55e1f2f15ce552da8fde2d
David Schmitt <david.schmitt@puppet.com>
2017-08-07 13:40:47 +0100

    (MAINT) enable HTML output for coverage reporting
    This allows useful local usage during development.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,10 @@ if ENV['COVERAGE'] == 'yes'
   require 'simplecov'
   require 'coveralls'
 
-  SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+  ]
   SimpleCov.start do
     add_filter '/spec/'
   end
